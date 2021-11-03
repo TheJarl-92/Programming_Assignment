af826c9b6039a7fec7aea7d3ee1cb658640a80a0
TheJarl-92 <robin.w.jarl@gmail.com>
2021-11-03 17:12:46 +0100

    Added description text to why I am using several break points when I first thought it might be an overuse of it.

--- src/WritingWordsMain.java
+++ src/WritingWordsMain.java
@@ -15,7 +15,11 @@ public class WritingWordsMain {
                 System.out.println("You wrote " + myWord.getCharacter() + " character(s) on " + myWord.getRow() + " row(s)" + " and " + myWord.getAmountOfWords() + " number of word(s).");
                 System.out.println("Your longest word was: " + myWord.getLongestWord());
                 break;
-
+                /*Har en break här och i for-loopen. Tyckte det verkade för mycket först men om jag tog från for-loopen
+                kunde jag inte räkna ut orden ordentligt. Om jag inte hade break i main metoden så körs programmet
+                i oändlighet, eller tills man stoppar det manuellt. Kändes snyggare att avsluta programmet om man väl
+                kom till ordet stop.
+                 */
             }
         }
     }
